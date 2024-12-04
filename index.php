9ac459870dc284deb7d611abe6f67b50e649797a
gonzalo <a23toblgo@iesgrancapitan.org>
2024-12-04 07:54:03 +0100

    04/12/2024

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Primero Script PHP que muestra el mensaje Hola Mundo
+ * 
+ * @author Gonzalo Torres <a23toblgo@iesgrancapitan.org>
+ * @category 01_ejBasicos
+ * @package iaw
+ * @version 1.0.0
+ * @license MIT
+ * 
+ */
+
+ echo "Hola Mundo";
